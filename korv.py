7c2984e3aaeec1966e38516052037208cbc36d35
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-09-01 09:53:10 +0200

    Fixat en bugg när det är ett jämt antal elever

--- korv.py
+++ korv.py
@@ -1,5 +1,7 @@
+import math
+
 print("---------------------------")
-print(".: KORVKOLLEN 1.0.1 :.")
+print(".: KORVKOLLEN 1.0.2 :.")
 print("---------------------------")
 print("Hur många elever vill ha... ")
 
@@ -8,8 +10,8 @@ _3_Korv = int(input("3 vanliga korvar     > "))
 _2_Vego = int(input("2 veganska korvar    > "))
 _3_Vego = int(input("3 veganska korvar    > "))
 
-korvPack = round((2 * _2_Korv + 3 * _3_Korv) / 8 + 0.5)
-vegoPack = round((2 * _2_Vego + 3 * _3_Vego) / 4 + 0.5)
+korvPack = math.ceil((2 * _2_Korv + 3 * _3_Korv) / 8)
+vegoPack = math.ceil((2 * _2_Vego + 3 * _3_Vego) / 4)
 drick = _2_Korv + _3_Korv + _2_Vego + _3_Vego
 
 pris = float(20.95 * korvPack + 34.95 * vegoPack + 13.95 * drick)
@@ -22,5 +24,5 @@ print("| Vegansk korv: ", vegoPack, "förpackningar")
 print("| Dryck:        ", drick, "drick")
 
 print("---------------------------")
-print("| ", pris, "SEK")
+print("| ", round(pris, 1), "SEK")
 print("---------------------------")
